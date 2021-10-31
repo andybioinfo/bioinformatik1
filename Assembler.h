9e063ac1946e1f5ded32a2fcad59a8e12b9906e9
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2021-11-01 00:35:32 +0100

    Methoden und Felder hinzugefügt

--- Assembler.h
+++ Assembler.h
@@ -12,11 +12,36 @@ class Assembler
 	using Seq = Sequence<Alphabet::DNA>;
 	using OGraph = Graph<Seq>;
 
+
+
+    /**
+     * Awesome static Method to build the awesome Overlay-Graph with a given Fasta-file.
+     * This method will return a File in Graphviz-Format
+     */
+    static void FromFastaFileToGraph(const char* inputfile,const char* outputfile);
+
+
 	/**
 	 * Creates an overlap-graph that contains the passed sequences as nodes.
 	 */
-	Assembler(const std::vector<Assembler::Seq>& sequences);
+	Assembler(const std::vector<Sequence<Alphabet::DNA>>& sequences);
+
+
+
+    /**
+     * returns the finished graph-instance
+     * */
+    OGraph getGraph();
+
+
+
+  private:
+
+    // Contains the finished Overlap-Graph
+    OGraph OverlapGraph;
+
 };
+
 
 #endif // BIOINFO1_ASSEMBLER_H
 
