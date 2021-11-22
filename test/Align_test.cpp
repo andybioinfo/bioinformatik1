cdb860ea06d0283b87eff0f51890e39fac715076
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2021-11-22 01:26:04 +0100

    Add files via upload

--- test/Align_test.cpp
+++ test/Align_test.cpp
@@ -13,16 +13,38 @@
 
 
 #include "../Alignment.h"
-#include "../EditDistance.h"
+#include "../Alignment.cpp"
 #include "../Distance.h"
 #include "../Matrix.h"
+
+#include "../sequence/Sequence.h"
+#include "../sequence/DNA.h"
+#include "../sequence/Peptide.h"
+#include "../sequence/RNA.h"
+#include "../sequence/DNA.cpp"
+#include "../sequence/Peptide.cpp"
+#include "../sequence/RNA.cpp"
 
 using namespace Alphabet;
 
 
-TEST(ALIGN, init)
+TEST(ALIGN, DNA_Align_Test)
 {
-	std::cout << "> GTAATC";
-    
+    Sequence<DNA> A = Sequence<DNA>::fromString("GGCTAAGGA");
+    Sequence<DNA> B = Sequence<DNA>::fromString("GTCAATATT");
 }
+
+TEST(ALIGN, PEPTIDE_Align_Test)
+{
+    Sequence<DNA> A = Sequence<DNA>::fromString("IWANTREALLYNICETASTYSVSHI");
+    Sequence<DNA> B = Sequence<DNA>::fromString("WALEAWANICESTSVSHITYHIHIIIHH");
+}
+
+TEST(ALIGN, RNA_Align_Test)
+{
+    Sequence<DNA> A = Sequence<DNA>::fromString("UAUUAGGC");
+    Sequence<DNA> B = Sequence<DNA>::fromString("UAAUUGCC");
+
+}
+
 
--- test/Matrix_test.cpp
+++ test/Matrix_test.cpp
@@ -13,9 +13,17 @@
 
 
 #include "../Alignment.h"
-#include "../EditDistance.h"
+#include "../Alignment.cpp"
 #include "../Distance.h"
 #include "../Matrix.h"
+
+#include "../sequence/Sequence.h"
+#include "../sequence/DNA.h"
+#include "../sequence/Peptide.h"
+#include "../sequence/RNA.h"
+#include "../sequence/DNA.cpp"
+#include "../sequence/Peptide.cpp"
+#include "../sequence/RNA.cpp"
 
 using namespace Alphabet;
 
