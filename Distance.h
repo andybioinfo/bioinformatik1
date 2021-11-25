7201b86766e42f560b49b9314e5edf6a32959f20
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2021-11-25 01:37:32 +0100

    Kommentare hinzugefügt

--- Distance.h
+++ Distance.h
@@ -2,24 +2,43 @@
 #define DISTANCE_H
 
 namespace Distance {
+
 /*
- * ???
+ * Distance class for alignment-algorithm
  * */
 template<typename Char>
 class EditDistance {
+
 public:
+
     int operator()(Char a,Char b);
+
     static bool is_A_minValue(int min_a, int other_b, int other_c);
+
 };
 
+/*
+ * Returns the Edit-Distance from a and b
+ * @a first character
+ * @b second character
+ * @return distance
+ * */
 template<typename Char>
-int EditDistance<Char>::operator()(Char a,Char b) {return a != b;}
+    int EditDistance<Char>::operator()(Char a,Char b) {
+        return a != b;
+    }
 
 
-
-    template<typename Char>
+/*
+ * Compare the value A with B and C
+ * @min_A the main value
+ * @min_B the other value
+ * @min_C the other value
+ * @return true, if A smaller/equal as B and C
+ * */
+template<typename Char>
     bool EditDistance<Char>::is_A_minValue(int min_A, int other_B, int other_C) {
-    if (min_A <= other_B && min_A <= other_C) {return true;}
+        if (min_A <= other_B && min_A <= other_C) {return true;}
     return false;
 }
 
