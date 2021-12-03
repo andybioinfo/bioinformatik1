35a5c44d26238a2cbc98a333df9ee01eea88e295
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2021-12-03 08:00:45 +0100

    Delete sequence directory

--- sequence/InvalidCharacter.cpp
+++
@@ -1,8 +0,0 @@
-#include "InvalidCharacter.h"
-
-InvalidCharacter::InvalidCharacter(char c)
-    : msg_(std::string("The character ") + c + " is not valid")
-{
-}
-
-const char* InvalidCharacter::what() const noexcept { return msg_.c_str(); }
--- sequence/ParsingException.cpp
+++
@@ -1,8 +0,0 @@
-#include "ParsingException.h"
-
-ParsingException::ParsingException(const std::string& msg)
-    : msg_(msg)
-{
-}
-
-const char* ParsingException::what() const noexcept { return msg_.c_str(); }
