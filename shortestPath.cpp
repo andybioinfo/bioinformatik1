872fefd9e781ab7c26ebaa4223af4007e82d9b29
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2021-12-03 14:32:24 +0100

    Update shortestPath.cpp

--- shortestPath.cpp
+++ shortestPath.cpp
@@ -13,5 +13,27 @@ using namespace std;
 
 int main(int   argc, char *argv[])
 {
+  
+  
+    console::Title("filefsd","A","B");
+
+    std::ifstream input("../matrix.txt");
+    if (!input) { console::Help("file error");
+        return 0;
+    }
+    input.close();
+
+    Graph G = Graph::createFromAdjacencyMatrixFile("../matrix.txt");
+
+    G.addToPath(&G.getNodes().at(0));
+    G.addToPath(&G.getNodes().at(1));
+    G.addToPath(&G.getNodes().at(2));
+    G.addToPath(&G.getNodes().at(3));
+    G.addToPath(&G.getNodes().at(2));
+    G.addToPath(&G.getNodes().at(0));
+
+    console::Matrix(G.getNodes().size(),G.getEdges().size(),"OK",true);
+
+    console::Result(G.getPathWeight(),G.getPath().size(),"A","B",G.pathToString());
   return 0;
 }
