650f5591476350fe030963c234e09ebc6e198d74
Mareike Syring <masy00001@stud.uni-saarland.de>
2021-12-09 19:58:13 +0100

    gurobi test

---
+++ test/Gurobi_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <initializer_list>
+#include <set>
+#include <vector>
+#include <deque>
+#include <string>
+#include <ostream>
+#include <istream>
+#include <sstream>
+#include <iostream>
+
+#include "Graph.h"
+#include "Gurobi.h"
+
+TEST(GUROBI_TEST, createFromAdjacencyMatrixFile){
+
+}
+
+TEST(GUROBI_TEST, startAlgorithm){
+
+}
+
+TEST(GUROBI_TEST, getPath){
+
+}
+
+TEST(GUROBI_TEST, pathToString){
+
+}
+
+TEST(GUROBI_TEST, addToPath){
+
+}
