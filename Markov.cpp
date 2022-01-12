b68174ab85171586a8e4659f2fe60bed0f32345e
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-01-12 02:13:06 +0100

    Aufteilung auf .cpp und .h

--- Markov.cpp
+++ Markov.cpp
@@ -1,2 +1,176 @@
 
-//#include "Markov.h"
+#include "Markov.h"
+
+/** set the value back in Range if it over 1.0 or under 0.0
+ *
+ * @val     value
+ * @return  value in Range
+ * */
+double Markov::Range(double val) {
+    if (val > 1.0) { return 1.0; }
+    if (val < 0.0) { return 0.0; }
+    return val;
+}
+
+
+/** Create from a String for every char { '0' -> Tail , '1' -> Head }
+ *  a Vector of Flips. It returns an empty Vector if an wrong char in the String.
+ *
+ * @str     a string in format "101001"
+ * @return  vector-list of Flip { Head , Tail}
+ * */
+std::vector<Flip> Markov::String2Sequence(string str) {
+    std::vector<Flip> seq;
+    for ( char& c : str) {
+        // 0 is Tail
+        if (c == '0') { seq.push_back(Tail); continue; }
+        // 1 is Head
+        if (c == '1') { seq.push_back(Head); continue; }
+        // other char = return empty List
+        seq.clear();
+        break;
+    }
+    return seq;
+}
+
+
+
+
+/** returns the change-probability from a Fair/Unfair to a Fair/Unfair coin
+ *
+ * @from   Fair or Unfair coin
+ * @to     Fair or Unfair coin
+ * @return resulting probability of this change
+ * */
+double Markov::changeProbability(Coin from, Coin to) {
+
+    // 0 = Unfair, 1 = Fair
+    switch (from) {
+        case Fair   : // from Fair
+            switch (to) {
+                case Fair   : return change.getValue(1,1);
+                case Unfair : return change.getValue(1,0);
+            }
+            break;
+        case Unfair : // from Unfair
+            switch (to) {
+                case Fair   : return change.getValue(0,1);
+                case Unfair : return change.getValue(0,0);
+            }
+            break;
+    }
+
+    return -1;
+}
+
+
+
+
+/** returns the Flip-probability's with a Fair/Unfair coin to Head or Tail
+ *
+ * @cointype   with a Fair or Unfair coin?
+ * @to         throw Head or Tail?
+ * @return     resulting FLip-probability of this throw
+ * */
+double Markov::prodProbability(Coin cointype, Flip to) {
+
+    // x0 = Tail, x1 = Head ; y0 = Unfair, y1 = Fair
+    switch (cointype) {
+        case Fair   : // from Fair
+            switch (to) {
+                case Tail   : return product.getValue(1,0);
+                case Head   : return product.getValue(1,1);
+            }
+            break;
+        case Unfair : // from Unfair
+            switch (to) {
+                case Tail   : return product.getValue(0,0);
+                case Head   : return product.getValue(0,1);
+            }
+            break;
+    }
+
+    return -1;
+}
+
+
+
+
+
+Markov::Markov(double p_coinchange, double p_head_fair, double p_head_unfair) {
+
+    // Create the matrix-values and set in Range
+    double _change = Range(p_coinchange);
+    double _same   = 1.0 - _change;
+
+    double _fair_head   = Range(p_head_fair);
+    double _fair_tail   = 1.0 - _fair_head;
+    double _unfair_head = Range(p_head_unfair);
+    double _unfair_tail = 1.0 - _unfair_head;
+
+    // Create change matrix
+    // 0 = Unfair, 1 = Fair
+    this->change.setValue(1,1,_same);
+    this->change.setValue(1,0,_change);
+    this->change.setValue(0,1,_change);
+    this->change.setValue(0,0,_same);
+
+    // Create production matrix
+    // x0 = Tail, x1 = Head ; y0 = Unfair, y1 = Fair
+    this->product.setValue(1,0,_fair_tail);
+    this->product.setValue(1,1,_fair_head);
+    this->product.setValue(0,0,_unfair_tail);
+    this->product.setValue(0,1,_unfair_head);
+
+
+}
+
+
+
+
+
+/** Print both matrices to console (std::cout)
+ *  for check all matrix-values
+ *
+ * */
+void Markov::print_matrices() {
+
+    char RESET[]   = "\033[0m";
+    char BBLUE[]   = "\033[1;34m";
+    char BWHITE[]  = "\033[1;37m";
+    char RED[]     = "\033[0;31m";
+    char BYELLOW[] = "\033[1;33m";
+
+    std::cout << BWHITE << "\n | Markov - Change-Matrix" << BBLUE; // 0 = Unfair, 1 = Fair
+    std::cout << RED    << "\n | Fair   -> Fair   : " << BYELLOW << change.getValue(1,1);
+    std::cout << RED    << "\n | Fair   -> Unfair : " << BYELLOW << change.getValue(1,0);
+    std::cout << RED    << "\n | Unfair -> Fair   : " << BYELLOW << change.getValue(0,1);
+    std::cout << RED    << "\n | Unfair -> Unfair : " << BYELLOW << change.getValue(0,0) << "\n";
+
+    std::cout << BWHITE << "\n | Markov - Production-Matrix" << BBLUE; // x0 = Tail, x1 = Head ; y0 = Unfair, y1 = Fair
+    std::cout << RED    << "\n | Fair   -> Head   : " << BYELLOW << product.getValue(1,1);
+    std::cout << RED    << "\n | Fair   -> Tail   : " << BYELLOW << product.getValue(1,0);
+    std::cout << RED    << "\n | Unfair -> Head   : " << BYELLOW << product.getValue(0,1);
+    std::cout << RED    << "\n | Unfair -> Tail   : " << BYELLOW << product.getValue(0,0) << RESET << "\n";
+
+}
+
+
+/** Default Constructor
+ *
+ * */
+Markov::Markov() {
+
+}
+
+
+/** (Getter for Testing) -> returns the change Matrix
+ *
+ * */
+Matrix Markov::getChangeMatrix()      { return change; }
+
+
+/** (Getter for Testing) -> returns the product Matrix
+ *
+ * */
+Matrix Markov::getProductMatrix()      { return product; }
--- Viterbi.cpp
+++ Viterbi.cpp
@@ -1,4 +1,243 @@
-//#include "Viterbi.h"
+#include "Viterbi.h"
 
+/** Returns the Sequence of the Coin-Flips, if with_result chosen,
+ *  the calculated Fair/Unfair throw will colorized in the String:
+ *   [Red   = unfair throw]
+ *   [Green = fair throw]
+ *   [White = unknown (=error) ]
+ *   (0 is Tail, 1 is Head)
+ *
+ * @with_result Colored fair-throw result
+ * @return The sequence in format { 01011 }
+ * */
+string Viterbi::Sequence_toString(bool with_result) {
+
+    string res = C::BWHITE;
+    res += "{ ";
+    int coin_pos = 0;
+
+    for (Flip f : sequence) {
+
+        if (with_result) {
+            if (result[coin_pos] == Fair) {res += C::BGREEN;}
+            else if (result[coin_pos] == Unfair) {res += C::BRED;}
+            else {res += C::BWHITE;}
+        }
+
+        if (f == Tail) {res += "0";}
+        if (f == Head) {res += "1";}
+
+        if (with_result) { res += C::BWHITE;
+            coin_pos ++ ;}
+
+    }
+
+    res += " }";res += C::RESET;
+
+    return res;
+}
+
+
+
+/** Calculate a cell from the matrix.
+ *
+ * @p_w_at_coin   probabilty of this FLip with this coin
+ * @decision      The Cointype of the biggest value of max1+2 will return in this list for backtracking
+ * @max_fair1     last value
+ * @max_fair2     prob. of change
+ * @max_unfair1   last value
+ * @max_unfair2   prob. of change
+ * */
+double Viterbi::formula(double p_w_at_coin, double max_fair1, double max_fair2, double max_unfair1, double max_unfair2, vector<Coin>& decision) {
+
+    double a = max_fair1 + log(max_fair2); // fair
+    double b = max_unfair1 + log(max_unfair2); // unfair
+
+    if (a > b) {decision.push_back(Fair);} else {decision.push_back(Unfair);}
+
+    double max_value = std::max(a,b);
+
+    return log(p_w_at_coin) + max_value;
+}
+
+
+
+
+
+
+
+/** Returns an Viterbi-Object ans create the HMM_Matrix inside and fill
+ *  his with the algorithm with values.
+ *
+ * @p_begin    Prob. for begin with the Fair Coin
+ * @p_unfair   Prob. for throw the Unfair Coin
+ * @p_change   Prob. for change between Coin{Fair,Unfair}
+ * @_sequence  The Coin-Throw sequence Values: Flip{Head, Tail}
+ * @return     Viterbi-Object with filled matrix
+ * */
+Viterbi::Viterbi(Markov _markov, double p_begin, vector<Flip> _sequence) {
+
+    // Check Arguments
+    if (_sequence.empty())   {throw std::invalid_argument("argument _sequence is empty!"); }
+
+    // Initialize Fields
+    this->_markov = _markov;
+    this->p_begin = p_begin;
+    this->sequence = _sequence;
+
+    // Initialize Matrix
+
+    this->M = Matrix(2,_sequence.size() );
+
+    // ## Start Algorithm (Fill Matrix)
+
+    // init
+
+    double last_fair   = 0.0; // at row 0 (y = 0)
+    double last_unfair = 0.0; // at row 1 (y = 1)
+    int actual_column  = 1;
+    int max_col        = _sequence.size()-1 ;
+    double pw_at_coin = 0.0;
+    double max_a1 = 0.0;
+    double max_a2 = 0.0;
+    double max_b1 = 0.0;
+    double max_b2 = 0.0;
+    double resA    = 0.0;
+    double resB    = 0.0;
+
+    Flip Xi = _sequence[0];
+
+    // column 1
+
+    last_fair   = log(p_begin)     + log(_markov.prodProbability(Fair,Xi));
+    last_unfair = log(1.0-p_begin) + log(_markov.prodProbability(Unfair,Xi));
+
+    M.setValue(0,0,last_fair);
+    M.setValue(1,0,last_unfair);
+
+    decision_resA.push_back(Fair);
+    decision_resB.push_back(Unfair);
+
+    // remaining colums
+
+    while (actual_column <= max_col) { // start next column
+
+        // Flip at Column
+
+        Xi = _sequence[actual_column];
+
+        // row 0 (fair)
+        pw_at_coin = _markov.prodProbability(Fair,Xi);
+        max_a1     = last_fair;
+        max_a2     = _markov.changeProbability(Fair,Fair);
+        max_b1     = last_unfair;
+        max_b2     = _markov.changeProbability(Unfair,Fair);
+
+        resA       = formula(pw_at_coin,max_a1,max_a2,max_b1,max_b2,decision_resA);
+
+        M.setValue(0,actual_column,resA);
+
+        // row 1 (unfair)
+        pw_at_coin = _markov.prodProbability(Unfair,Xi);
+        max_a1     = last_fair;
+        max_a2     = _markov.changeProbability(Fair,Unfair);
+        max_b1     = last_unfair;
+        max_b2     = _markov.changeProbability(Unfair,Unfair);
+
+        resB       = formula(pw_at_coin,max_a1,max_a2,max_b1,max_b2,decision_resB);
+
+        M.setValue(1,actual_column,resB);
+
+        // set last values for next column
+
+        last_fair   = resA;
+        last_unfair = resB;
+
+        // continue to next column
+        actual_column++;;
+    }
+
+    M.print();
+
+}
+
+
+
+
+
+/** Backtracking in matrix for create the Result-Sequence of Coin{Fair,Unfair}
+ *  for evaluate the Input/Throw-Sequence.
+ *
+ * @return     nix
+ * */
+void Viterbi::backtracking() {
+
+    /* let this cat show you, where the most
+       likely path is through this matrix
+
+                 (meow!)
+         /\_/\   )/
+   ((   (=^.^=)
+   ))    )   (
+    (( /      \    ' '
+     (   ) || ||     ' '
+     '----''-''-'  >+++°>
+
+    */
+
+    // set the position on end of the input-sequence
+    int actual_column = sequence.size() - 1;
+
+    // read first value in the actual column
+    double row_A = M.getValue(0,actual_column);
+
+    // read second value in the actual column
+    double row_B = M.getValue(1,actual_column);
+
+    // Set row/coin cursor
+    Coin actual;
+    if (row_A > row_B) { actual = Fair; } else { actual = Unfair; }
+
+    // Start backtracker
+    while (actual_column >= 0) { // start at last column and move to first column
+
+        // Add Type to Result-List
+        result.push_back(actual);
+
+        // Jump to Cell of the last max of Fair
+        if (actual == Fair)   { actual = decision_resA[actual_column];
+            actual_column--;
+            continue;
+        }
+
+        // Jump to Cell of the last max of Unfair
+        if (actual == Unfair) { actual = decision_resB[actual_column];
+            actual_column--;
+            continue;
+        }
+        actual_column--;
+    }
+
+    // reverse the backtracking-list
+
+    std::reverse(result.begin(),result.end());
+
+    // ##
+
+}
+
+
+
+
+/** Getter for Testing
+ *
+ * */
+Matrix Viterbi::getMatrix()      { return M; }
+double Viterbi::get_p_begin()    { return p_begin; }
+std::vector<Flip> Viterbi::get_Sequence() { return sequence;}
+std::vector<Coin> Viterbi::get_Result() { return result;}
+std::vector<Coin> Viterbi::get_Decision_resA() { return decision_resA;}
+std::vector<Coin> Viterbi::get_Decision_resB() { return decision_resB;}
+Markov Viterbi::get_markov_matrices() { return _markov;}
 
 
