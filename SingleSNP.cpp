f5f967d10bd897acc458a0370c2a45716556ffbf
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-01-21 16:47:08 +0100

    Add files via upload

--- SingleSNP.cpp
+++ SingleSNP.cpp
@@ -54,7 +54,7 @@ double SingleSNP::computeF() {
     double pY1_Xi1 =    probability(Cancer,Hetero);
     double pY1_Xi2 =    probability(Cancer,HomoMinor);
 
-    //std::cout << "p(y0): " << pY0 << " p(y1): " << pY1 << " p(x0): " <<pXi0 << " p(x1): " <<pXi1 << " p(x2): " <<pXi2 << " p(y0x0): " <<pY0_Xi0 << " p(y0x1): " <<pY0_Xi1 << " p(y0x2): " <<pY0_Xi2 << " p(y1x0): " <<pY1_Xi0 << " p(y1x1): " <<pY1_Xi1 << " p(y1x2): " <<pY1_Xi2 << "||";
+    //std::cout << "p(y0): " << pY0 << " p(y1): " << pY1 << " p(x0): " <<pXi0 << " p(x1): " <<pXi1 << " p(x2): " <<pXi2 << " p(y0x0): " <<pY0_Xi0 << " p(y0x1): " <<pY0_Xi1 << " p(y0x2): " <<pY0_Xi2 << " p(y1x0): " <<pY1_Xi0 << " p(y1x1): " <<pY1_Xi1 << " p(y1x2): " <<pY1_Xi2 << " || ";
 
     double _IXY =   I_XY_Formula(pY0,pY1,pXi0,pXi1,pXi2,pY0_Xi0,pY0_Xi1,pY0_Xi2,pY1_Xi0,pY1_Xi1,pY1_Xi2);
     double _HX =    H_X_Formula(pXi0,pXi1,pXi2);
@@ -73,7 +73,7 @@ double SingleSNP::computeF() {
 // I(X,Y)
 double SingleSNP::I_XY_Formula(double pY0, double pY1, double pXi0, double pXi1, double pXi2, double pY0_Xi0, double pY0_Xi1,
                         double pY0_Xi2, double pY1_Xi0, double pY1_Xi1, double pY1_Xi2) {
-
+    double res = 0;
     double s1 = pY0_Xi0 * log(pY0_Xi0 / ( pY0 * pXi0 ) );
     double s2 = pY0_Xi1 * log(pY0_Xi1 / ( pY0 * pXi1 ) );
     double s3 = pY0_Xi2 * log(pY0_Xi2 / ( pY0 * pXi2 ) );
@@ -81,17 +81,29 @@ double SingleSNP::I_XY_Formula(double pY0, double pY1, double pXi0, double pXi1,
     double s5 = pY1_Xi1 * log(pY1_Xi1 / ( pY1 * pXi1 ) );
     double s6 = pY1_Xi2 * log(pY1_Xi2 / ( pY1 * pXi2 ) );
 
-    return s1 + s2 + s3 + s4 + s5 + s6;
+    if (s1 == s1) {res += s1;}
+    if (s2 == s2) {res += s2;}
+    if (s3 == s3) {res += s3;}
+    if (s4 == s4) {res += s4;}
+    if (s5 == s5) {res += s5;}
+    if (s6 == s6) {res += s6;}
+
+
+    return res;
 }
 
 // H(X)
 double SingleSNP::H_X_Formula(double pXi0, double pXi1, double pXi2) {
 
+    double res = 0;
     double s1 = pXi0 * log( 1 /  pXi0  );
     double s2 = pXi1 * log( 1 /  pXi1  );
     double s3 = pXi2 * log( 1 /  pXi2  );
+    if (s1 == s1) {res += s1;}
+    if (s2 == s2) {res += s2;}
+    if (s3 == s3) {res += s3;}
 
-    return s1 + s2 + s3;
+    return res;
 }
 
 
