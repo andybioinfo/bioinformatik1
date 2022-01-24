4c55d4cb03e07c5e5b781824ad3ca3cc8c2057af
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-01-24 05:30:11 +0100

    Add files via upload

--- test/Formula_test.cpp
+++ test/Formula_test.cpp
@@ -1,9 +1,10 @@
 //
 // Created by manjaro on 21.01.22.
 //
-#include <gtest/gtest.h>
 
+#include <gtest/gtest.h>
 #include "../Snipper.h"
+#include "../SingleSNP.h"
 
 
 // Builder for "Exercise 4 SNPs"
@@ -23,16 +24,16 @@ Snipper getExercise4SNPs() {
 
 
 
-
+// Tests the I(X,Y) Formula
 TEST(SNP_FORMULA, I_XY_FormulaTest)
 {
 
-    double pY0 = 2.0/6.0;double pY1 = 4.0/6.0;
+    double pY0 = 2.0/6.0;double pY1 = 4.0/6.0;                                          // Exercise 4
 
-    double pXi0 = 2.0/6.0;double pXi1 = 2.0/6.0;double pXi2 = 2.0/6.0;
+    double pXi0 = 2.0/6.0;double pXi1 = 2.0/6.0;double pXi2 = 2.0/6.0;                  // Exercise 4
 
-    double pY0_Xi0 = 0;         double pY0_Xi1 = 0;         double pY0_Xi2 = 2.0/6.0;
-    double pY1_Xi0 = 2.0/6.0;   double pY1_Xi1 = 2.0/6.0;   double pY1_Xi2 =    0;
+    double pY0_Xi0 = 0;         double pY0_Xi1 = 0;         double pY0_Xi2 = 2.0/6.0;   // Exercise 4
+    double pY1_Xi0 = 2.0/6.0;   double pY1_Xi1 = 2.0/6.0;   double pY1_Xi2 =    0;      // Exercise 4
 
     double X1 = SingleSNP::I_XY_Formula(pY0,pY1,pXi0,pXi1,pXi2,pY0_Xi0,pY0_Xi1,pY0_Xi2,pY1_Xi0,pY1_Xi1,pY1_Xi2);
 
@@ -50,26 +51,54 @@ TEST(SNP_FORMULA, I_XY_FormulaTest)
 
     double X3 = SingleSNP::I_XY_Formula(pY0,pY1,pXi0,pXi1,pXi2,pY0_Xi0,pY0_Xi1,pY0_Xi2,pY1_Xi0,pY1_Xi1,pY1_Xi2);
 
-    EXPECT_EQ(0.2764345909, X1);
-    EXPECT_EQ(0.1760912591, X2);
-    EXPECT_EQ(0, X3);
+    EXPECT_EQ(0.2764345909, X1); // Exercise 4
+    EXPECT_EQ(0.1760912591, X2); // Exercise 4
+    EXPECT_EQ(0           , X3); // Exercise 4
 
 }
 
 
 
+// Tests the H(X) Formula
+TEST(SNP_FORMULA, H_X_FormulaTest)
+{
+
+    double pY0 = 2.0/6.0; 
+    double pY1 = 4.0/6.0;
+
+    double H1 = SingleSNP::H_X_Formula(pY0,pY1); // Exercise 4
+
+    pY0 = 0.0/6.0; 
+    pY1 = 6.0/6.0;
+
+    double H2 = SingleSNP::H_X_Formula(pY0,pY1); // Zero Test p(Y1)=0
+
+    pY0 = 6.0/6.0; 
+    pY1 = 0.0/6.0;
+
+    double H3 = SingleSNP::H_X_Formula(pY0,pY1); // Zero Test p(Y0)=0
+
+    EXPECT_EQ(0.2764345909 , X1);
+    EXPECT_EQ(0            , X2);
+    EXPECT_EQ(0            , X3);
+
+}
+
+
+
+// Tests the computeF-Method
 TEST(SNP_FORMULA, ComputeF_Test)
 {
 
-Snipper S = getExercise4SNPs()
+Snipper S = getExercise4SNPs(); // Exercise 4
 
     double X1 = S[0].computeF();
     double X2 = S[1].computeF();
     double X3 = S[2].computeF();
 
-    EXPECT_EQ(0.51, X1);
-    EXPECT_EQ(0.51, X2);
-    EXPECT_EQ(0.51, X3);
+    EXPECT_EQ(0        , X1);
+    EXPECT_EQ(0.637009 , X2);
+    EXPECT_EQ(0        , X3);
 
 }
 
