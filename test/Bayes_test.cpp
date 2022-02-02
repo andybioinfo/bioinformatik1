0cc782105ba0572aed6ac57162c525dfb1aad88f
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-02-02 13:05:25 +0100

    Update Bayes_test.cpp

--- test/Bayes_test.cpp
+++ test/Bayes_test.cpp
@@ -33,6 +33,27 @@ TEST(Model, Test)
     }
 
 
-      
+      // Create Naive Bayes - Testing/Predict
+void test7() {
+
+    Snipper S = createExample2();  // create premixed Database
+
+    NaiveBayes NB(S, 10,false);  
+
+    std::vector<Block*> training = {&NB.getK_Blocks()[0] , &NB.getK_Blocks()[1] , &NB.getK_Blocks()[2]};
+
+    NB.train(S,training);
+
+    NB.getK_Blocks()[3].predict(NB.getModel(),S);
+
+ // ## Check predictions:
+
+    NB.getK_Blocks()[3].getBlockPredictions()[0]; // Cancer
+    NB.getK_Blocks()[3].getBlockPredictions()[1]; // Cancer
+    NB.getK_Blocks()[3].getBlockPredictions()[2]; // Cancer
+    NB.getK_Blocks()[3].getBlockPredictions()[3]; // 
+    NB.getK_Blocks()[3].getBlockPredictions()[4]; // Cancer
+}
+
 
 
